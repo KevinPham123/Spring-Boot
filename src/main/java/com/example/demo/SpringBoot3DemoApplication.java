78fac6232a1accc1f3b24337faef79b897cb6fd7
KevinPham123 <54450659+KevinPham123@users.noreply.github.com>
2023-09-20 23:05:53 -0500

    Actuator Endpoint permitting all access

--- src/main/java/com/example/demo/SpringBoot3DemoApplication.java
+++ src/main/java/com/example/demo/SpringBoot3DemoApplication.java
@@ -1,11 +1,12 @@
 package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-@SpringBootApplication
-@EnableWebMvc
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class, ManagementWebSecurityAutoConfiguration.class })
 public class SpringBoot3DemoApplication {
 
 	public static void main(String[] args) {
